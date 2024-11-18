9fe565377733ccad9062136502db2e91c3d032d9
Rugved-Technify <rugvedr39@gmail.com>
2024-11-18 14:44:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
