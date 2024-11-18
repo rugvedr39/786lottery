2afdba08da1f04b13d97a99ae58a489faee514f1
Rugved-Technify <rugvedr39@gmail.com>
2024-11-18 14:27:00 +0530

    edited config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -3,17 +3,11 @@ module.exports = {
         {
             name: "venasclub",
             script: "./src/server.js",
-            env_production: {
-                NODE_ENV: "production",
-            },
             interpreter: "node",
         },
         {
             name: "venasclub-jili",
             script: "./src/index.js",
-            env_production: {
-                NODE_ENV: "production",
-            },
             interpreter: "node",
         },
     ],
